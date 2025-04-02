3087fc799609e936267b87fc688bb53d0cbf602f
Tehran Doroudchi <tehrandoroudchi@gmail.com>
2025-04-02 13:45:33 +0200

    Fix cors

--- IPA-backend/app.js
+++ IPA-backend/app.js
@@ -7,7 +7,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:4000",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "X-API-KEY"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
